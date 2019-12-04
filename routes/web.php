e6fb9f7ec32601e22041a19a4cf2a325325574d8
Alexander Zodov <alexander.zodov@scalors.com>
2019-12-04 11:07:26 +0200

    feat(Routes): ADD routes via Resources groups for test-api

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,41 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::group(['prefix' => 'test-api', 'as' => 'api::', 'middleware'=>'web'], function () {
+
+    Route::resource('author', 'AuthorController', [
+        'only' => ['index', 'store', 'show', 'update', 'destroy'],
+        'names' => [
+            'index' => 'author::index',
+            'show' => 'author::show',
+            'store' => 'author::store',
+            'update' => 'author::update',
+            'destroy' => 'author::delete'
+        ]
+    ]);
+
+    Route::resource('category', 'CategoryController', [
+        'only' => ['index', 'store', 'show', 'update', 'destroy'],
+        'names' => [
+            'index' => 'category::index',
+            'show' => 'category::show',
+            'store' => 'category::create',
+            'update' => 'category::update',
+            'destroy' => 'category::delete'
+        ]
+    ]);
+
+    Route::resource('book', 'BookController', [
+        'only' => ['index', 'store', 'show', 'update', 'destroy'],
+        'names' => [
+            'index' => 'book::index',
+            'show' => 'book::show',
+            'store' => 'book::create',
+            'update' => 'book::update',
+            'destroy' => 'book::delete'
+        ]
+    ]);
+
+
+});
